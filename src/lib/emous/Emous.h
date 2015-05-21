951ab3918ff7eb474a731d03bdde0e35237c9f25
Marcel Hollerbach <marcel-hollerbach@t-online.de>
2015-05-21 10:04:33 +0200

    emous: Eo is not beta anymore /o\

--- src/lib/emous/Emous.h
+++ src/lib/emous/Emous.h
@@ -5,14 +5,9 @@
 #include <Eo.h>
 
 #ifdef EFL_EO_API_SUPPORT
-# ifndef EFL_BETA_API_SUPPORT
-#   error "You have defined EO_API_SUPPORT but not BETA_API_SUPPORT this will break the world"
-# else
-
-#   include "emous_device.eo.h"
-#   include "emous_device_type.eo.h"
-#   include "emous_manager.eo.h"
-# endif
+# include "emous_device.eo.h"
+# include "emous_device_type.eo.h"
+# include "emous_manager.eo.h"
 #else
 # include "emous_device.eo.legacy.h"
 # include "emous_device_class.eo.legacy.h"
