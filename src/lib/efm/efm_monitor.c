016c27b38cf42b7a6ff6a1a775310dd542cfaa13
Marcel Hollerbach <marcel-hollerbach@t-online.de>
2015-05-22 15:14:59 +0200

    efm_lib: call destructor correctly

--- src/lib/efm/efm_monitor.c
+++ src/lib/efm/efm_monitor.c
@@ -183,5 +183,6 @@ _efm_monitor_eo_base_destructor(Eo *obj EINA_UNUSED, Efm_Monitor_Data *pd)
    eina_stringshare_del(pd->directory);
 
    eina_hash_free(pd->file_icons);
+   eo_do_super(obj, EFM_MONITOR_CLASS, eo_destructor());
 }
 #include "efm_monitor.eo.x"
