05cc6ad0856f79d094cda5822a7a960683d06609
Marcel Hollerbach <marcel-hollerbach@t-online.de>
2015-05-22 14:41:42 +0200

    efm_lib: Add internal funcs which is handing eio_monitor event

--- src/lib/efm/efm_priv.h
+++ src/lib/efm/efm_priv.h
@@ -8,48 +8,17 @@
 #include <Ecore_File.h>
 #include <Efreet_Mime.h>
 
-struct _EFM_Monitor
-{
-   const char *directory; //< The directory this monitor listens to
-   Eio_Monitor *mon; //< The eio monitor which is started in the directory
-   Eio_File *file; //< The eio file as long as the ls is running
-
-   Eina_Hash *file_icons; // < Hash table of all listed FM_Monitor_Files
-   File_Cb add_cb, del_cb, mime_ready_cb; // < The callbacks to call when something happen
-   Err_Cb selfdel_cb, err_cb; //^
-   void *data;
-   Eina_Bool only_folder, hidden_files; //< the configuration which files to use
-   Ecore_Thread *mime_thread; //< the thread which is querying the mime types
-   Ecore_Idler *thread_idler; //< idler to start the thread
-   struct {
-      Eina_List *add; //< the list which icons to query
-      Eina_Lock lock;
-   } thread;
-
-   Eina_Bool deletion_mark;
-};
-
-struct _EFM_File{
-  Eina_Bool exists; //< true if the file exists at the add event
-  Eina_Bool writeable; //< true if the file is writeabke
-  Eina_Bool dir; //< true if the file is a directory
-  Eina_Bool use; //< true if the filter returned true for the file
-
-  struct stat stat;
-
-  const char *file; //< file name of the path
-  const char *path; //< the complete path
-
-  const char *fileending; //< the file ending of the file
-
-  const char *mime_type;
-};
-
 struct _EFM_Mount_Fs{
    const char *name;
    const char *script_path;
    const char *description;
 };
+
+typedef enum {
+    ADD,DEL,ERROR,
+} Fm_Action;
+
+typedef void (*Fm_File_Action)(void *data, Efm_Monitor *mon, const char *file, Fm_Action add);
 
 extern int _log_domain;
 
@@ -60,5 +29,7 @@ extern int _log_domain;
 #define DBG(...)      EINA_LOG_DOM_DBG(_log_domain, __VA_ARGS__)
 
 int fm_monitor_init();
+void fm_monitor_add(Efm_Monitor *mon, Eio_Monitor *monitor, Fm_File_Action action);
+void fm_monitor_del(Efm_Monitor *mon, Eio_Monitor *monitor);
 void fm_monitor_shutdown();
 #endif
