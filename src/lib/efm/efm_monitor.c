8ea1e915e9359e813ff396af305dbd97d92b6855
Marcel Hollerbach <marcel-hollerbach@t-online.de>
2015-05-24 19:02:48 +0200

    efm_monitor: does not need stop anymore
    nothing else than stop is called in there

--- src/lib/efm/efm_monitor.c
+++ src/lib/efm/efm_monitor.c
@@ -287,13 +287,6 @@ _efm_monitor_start(Eo *obj EINA_UNUSED, void *data EINA_UNUSED, const char *dire
    return mon;
 }
 
-EOLIAN static void
-_efm_monitor_stop(Eo *obj, Efm_Monitor_Data *pd)
-{
-   eo_del(obj);
-}
-
-
 static void
 _efm_monitor_eo_base_destructor(Eo *obj EINA_UNUSED, Efm_Monitor_Data *pd)
 {
