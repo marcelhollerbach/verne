c2c61da6f6ff383747cfe27fccbf3e029da46627
Marcel Hollerbach <marcel-hollerbach@t-online.de>
2015-07-07 20:23:04 +0200

    efm: drop out old archive code

--- src/lib/efm/Efm.h
+++ src/lib/efm/Efm.h
@@ -27,15 +27,6 @@ EAPI int efm_init();
  * shutdown the fm_monitor
  */
 EAPI void efm_shutdown();
-/*
- * Efm archive stuff
- */
-
-EAPI Eina_Bool efm_archive_file_supported(const char *fileending);
-
-EAPI Eina_Bool efm_archive_file_extract(const char *file, const char *goal);
-
-EAPI const char *efm_archive_lasterror_get(void);
 
 #undef EAPI
 
