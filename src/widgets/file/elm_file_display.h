19e63af5907cc533ac07b799d3eb0a940132f4a8
Marcel Hollerbach <marcel-hollerbach@t-online.de>
2015-07-13 18:19:33 +0200

    Get rid of the old api files!

--- src/widgets/file/elm_file_display.h
+++ src/widgets/file/elm_file_display.h
@@ -8,11 +8,6 @@
 #include "views/elm_file_display_view_debug.eo.h"
 #include "views/elm_file_display_view_grid.eo.h"
 #include "views/elm_file_display_view_list.eo.h"
-
-#ifdef EFL_EO_API_SUPPORT
-#   include "elm_file_display.eo.h"
-#else
-#   include "elm_file_display.eo.legacy.h"
-#endif
+#include "elm_file_display.eo.h"
 
 #endif
