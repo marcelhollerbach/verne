2d5d463b21fcff534c17c017f2af4a2f50aa9abd
Marcel Hollerbach <marcel-hollerbach@t-online.de>
2015-08-13 18:19:59 +0200

    elm_file_display: Use the new 3 widgets to assemle the same than the old

--- src/widgets/file/elm_file_display_priv.h
+++ src/widgets/file/elm_file_display_priv.h
@@ -26,45 +26,9 @@
 
 extern Elm_File_MimeType_Cache *cache;
 
-typedef struct
-{
-   Eo *obj;
-
-   const Eo_Class *view_klass;
-   Evas_Object *cached_view;
-
-   const char* current_path;
-
-   Evas_Object *bookmarks;
-
-   struct event {
-     Evas_Object *rect, *selection;
-     Evas_Point mouse_down;
-     Eina_Bool on_item;
-   }event;
-
-   Eina_List *selection;
-   void *drag_data;
-   Eina_Bool show_bookmark;
-   Eina_Bool show_filepreview;
-   Evas_Object *preview;
-   Evas_Object *bookmark;
-
-   struct search {
-     Ecore_Timer *clear_timer;
-     Eina_Strbuf *searchpart;
-     const char *pass;
-   } search;
-
-} Elm_File_Display_Data;
-
 // == calls which are just calling the cb of the view, but secure
 
 Evas_Object* icon_create(Evas_Object *par, Efm_File *file);
-
-// event helper for selection
-Eina_Bool _util_item_select_simple(void *data, Eo *obj, const Eo_Event_Description *desc, void *event);
-Eina_Bool _util_item_select_choosen(void *data, Eo *obj, const Eo_Event_Description *desc, void *event);
 
 /*
  * Sort helper function
