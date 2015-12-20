19ac05977f4cd12cef78c01fb91ecb4ec4b43846
Marcel Hollerbach <marcel-hollerbach@t-online.de>
2015-12-20 13:50:00 +0100

    archive: update doc

--- src/lib/efm/archive.h
+++ src/lib/efm/archive.h
@@ -6,8 +6,8 @@ int archive_shutdown(void);
 
 /*
  * Start to access a directory,
- * if directly is 1 the call will return when the archive has finished  extracting
- *
+ * if directly is 1 the call will return when the archive has finished extracting
+ * if it is 0 the function will extract asynchrone
  */
 const char* archive_access(const char* archive, int directly);
 
