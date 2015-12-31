0910c4398f1fe6ceaf5ba01b853cdcf9db18cf27
Marcel Hollerbach <marcel-hollerbach@t-online.de>
2015-12-31 12:10:32 +0100

    i should not forget them.

---
+++ src/lib/emous/config.h.cmake
@@ -0,0 +1,6 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#cmakedefine EMOUS_MODULE_PATH @EMOUS_MODULE_PATH@
+
+#endif // CONFIG_H
---
+++ src/widgets/config.h.cmake
@@ -0,0 +1,7 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#cmakedefine THEME_PATH @THEME_PATH@
+#cmakedefine DEBUG
+
+#endif // CONFIG_H
