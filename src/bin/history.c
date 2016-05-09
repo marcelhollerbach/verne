9b015bfad07ce2eadbb85518f1266b473583b55b
Marcel Hollerbach <marcel-hollerbach@t-online.de>
2016-05-09 22:51:15 +0200

    jesus: rework history

--- src/bin/history.c
+++ src/bin/history.c
@@ -1,81 +1,91 @@
 #include "jesus.h"
 
 static Eina_List *list = NULL;
-static unsigned int pointer;
+static unsigned int pointer = 0;
 static Eina_Bool barrier = EINA_FALSE;
 
 static void
-_back_cb(void *data EINA_UNUSED, Evas_Object *obj EINA_UNUSED, const char *emission EINA_UNUSED, const char *source EINA_UNUSED)
+_history_push(Efm_File *file)
+{
+   Eina_Stringshare *path;
+
+   path = eina_stringshare_add(
+    efm_file_path_get(file));
+
+
+   list = eina_list_append(list, path);
+}
+
+static void
+_history_pop(void)
+{
+   Eina_List *last;
+
+   last = eina_list_last(list);
+
+   eo_unref(last->data);
+
+   list = eina_list_remove_list(list, last);
+
+}
+
+static const char*
+_history_get(void)
+{
+   return eina_list_last_data_get(list);
+}
+
+static const char*
+_history_neg(int i)
+{
+   return eina_list_nth(list, eina_list_count(list) - (i + 1));
+}
+
+static void
+_flush(void)
 {
     const char *path;
     Efm_File *file;
 
-    if (pointer < 1)
-      return;
-
-    pointer --;
-
-    path = eina_list_nth(list, pointer);
-    barrier = EINA_TRUE;
+    path = _history_neg(pointer);
     file = efm_file_get(EFM_CLASS, path);
+
+    barrier = EINA_TRUE;
     elm_file_selector_file_set(selector, file);
     barrier = EINA_FALSE;
 }
 
 static void
+_back_cb(void *data EINA_UNUSED, Evas_Object *obj EINA_UNUSED, const char *emission EINA_UNUSED, const char *source EINA_UNUSED)
+{
+   if (pointer + 1 == eina_list_count(list)) return;
+
+   pointer++;
+   _flush();
+}
+
+static void
 _forward_cb(void *data EINA_UNUSED, Evas_Object *obj EINA_UNUSED, const char *emission EINA_UNUSED, const char *source EINA_UNUSED)
 {
-   const char *pointed;
-   Efm_File *file;
+   if (pointer == 0) return;
 
-   if (pointer == eina_list_count(list) + 1)
-     return;
-
-   pointer ++;
-
-   pointed = eina_list_nth(list, pointer);
-
-   file = efm_file_get(EFM_CLASS, pointed);
-   elm_file_selector_file_set(selector, file);
+   pointer --;
+   _flush();
 }
 
 static Eina_Bool
 _path_changed_cb(void *data EINA_UNUSED, const Eo_Event *event)
 {
-   const char *pointed;
-   const char *share;
-   const char *path;
+   if (barrier) return EO_CALLBACK_CONTINUE;
+   if (event->info == _history_get()) return EO_CALLBACK_CONTINUE;
 
-   path = efm_file_path_get(event->info);
-
-   if (barrier)
-     return EINA_TRUE;
-
-   if (eina_list_count(list) > pointer +1)
+   while(pointer > 0)
      {
-        pointed = eina_list_nth(list, pointer + 1);
-
-        if (pointed && !strcmp(pointed , path))
-          {
-             //
-             pointer ++;
-             return EINA_TRUE;
-          }
-        // unpop the upper items
-        while(eina_list_count(list) > pointer + 1)
-          {
-             const char *item;
-
-             item = eina_list_data_get(eina_list_last(list));
-             list = eina_list_remove(list, item);
-             eina_stringshare_del(item);
-          }
+        pointer --;
+        _history_pop();
      }
 
-   share = eina_stringshare_add(path);
-   list = eina_list_append(list, share);
-
-   pointer = eina_list_count(list) - 1;
+   _history_push(event->info);
 
    return EINA_TRUE;
 }
