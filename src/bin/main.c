48e9e5de549fe63ce32a95ae5b924070d6c2244d
Marcel Hollerbach <marcel-hollerbach@t-online.de>
2017-05-02 23:33:04 +0200

    take 2

--- src/bin/main.c
+++ src/bin/main.c
@@ -95,7 +95,7 @@ ui_init()
 int
 elm_main(int argc EINA_UNUSED, char **argv EINA_UNUSED)
 {
-   char *path = NULL;
+   /*char *path = NULL;
    Efm_File *file;
    // check if someone gave us a path
    if (argc > 2)
@@ -171,6 +171,23 @@ elm_main(int argc EINA_UNUSED, char **argv EINA_UNUSED)
    config_shutdown();
 
    efm_shutdown();
-   return 0;
+   return 0;*/
+   {
+         Evas_Object *win;
+
+         elm_policy_set(ELM_POLICY_QUIT, ELM_POLICY_QUIT_LAST_WINDOW_CLOSED);
+
+         win = elm_win_util_standard_add("Main", "Hello, World!");
+         elm_win_autodel_set(win, EINA_TRUE);
+         //win 400x400
+         evas_object_resize(win, 400, 400);
+
+         /*basic tutorial code*/
+
+         evas_object_show(win);
+
+         elm_run();
+         return 0;
+   }
 }
 ELM_MAIN()
