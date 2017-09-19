22096ea33597bf6ec49ce1cf76278c5aa4336bea
Marcel Hollerbach <marcel-hollerbach@t-online.de>
2017-09-19 10:14:49 +0200

    unjpeg ui.c

--- src/bin/ui.c
+++ src/bin/ui.c
@@ -38,7 +38,7 @@ open_with2_main_win_create(Eo *__main_parent EINA_UNUSED)
    main_win = elm_win_util_standard_add("Open with", "");
    pub_widgets->main_win = main_win;
    efl_gfx_size_hint_weight_set (main_win, 1.000000, 1.000000);
-   efl_gfx_size_set (main_win, 454, 379);
+   evas_object_move (main_win, 454, 379);
    efl_ui_win_autodel_set (main_win, EINA_TRUE);
    elm_win_title_set(main_win, "Open with");
    elm_win1_main_menu = elm_menu_add(main_win);
@@ -47,12 +47,12 @@ open_with2_main_win_create(Eo *__main_parent EINA_UNUSED)
    efl_gfx_size_hint_weight_set (elm_box1, 1.000000, 1.000000);
    efl_gfx_visible_set (elm_box1, EINA_TRUE);
    efl_gfx_size_hint_align_set (elm_box1, -1.000000, -1.000000);
-   efl_gfx_position_set (elm_box1, 72, 86);
+   evas_object_move (elm_box1, 72, 86);
 
    elm_genlist1 = elm_genlist_add(elm_box1);
    pub_widgets->elm_genlist1 = elm_genlist1;
    efl_gfx_visible_set (elm_genlist1, EINA_TRUE);
-   efl_gfx_size_set (elm_genlist1, 70, 60);
+   evas_object_move (elm_genlist1, 70, 60);
    efl_gfx_size_hint_weight_set (elm_genlist1, 1.000000, 1.000000);
    efl_gfx_size_hint_align_set (elm_genlist1, -1.000000, -1.000000);
 
@@ -67,7 +67,7 @@ open_with2_main_win_create(Eo *__main_parent EINA_UNUSED)
    pub_widgets->open = open;
    efl_gfx_size_hint_weight_set (open, 1.000000, 1.000000);
    efl_gfx_visible_set (open, EINA_TRUE);
-   efl_gfx_size_set (open, 73, 30);
+   evas_object_move (open, 73, 30);
    efl_gfx_size_hint_align_set (open, -1.000000, -1.000000);
    elm_object_part_text_set (open, NULL, "Open");
 
@@ -75,7 +75,7 @@ open_with2_main_win_create(Eo *__main_parent EINA_UNUSED)
    pub_widgets->asdefault = asdefault;
    efl_gfx_size_hint_weight_set (asdefault, 1.000000, 1.000000);
    efl_gfx_visible_set (asdefault, EINA_TRUE);
-   efl_gfx_size_set (asdefault, 73, 30);
+   evas_object_move (asdefault, 73, 30);
    efl_gfx_size_hint_align_set (asdefault, -1.000000, -1.000000);
    elm_object_part_text_set (asdefault, NULL, "Set as default");
    elm_box_pack_end (elm_box2, asdefault);
@@ -89,7 +89,7 @@ open_with2_main_win_create(Eo *__main_parent EINA_UNUSED)
 
    elm_label1 = elm_label_add(elm_box3);
    efl_gfx_visible_set (elm_label1, EINA_TRUE);
-   efl_gfx_size_set (elm_label1, 60, 30);
+   evas_object_move (elm_label1, 60, 30);
    efl_gfx_size_hint_weight_set (elm_label1, 1.000000, 0.000000);
    efl_gfx_size_hint_align_set (elm_label1, -1.000000, 0.500000);
    elm_object_part_text_set(elm_label1, NULL, "Current default:");
@@ -99,14 +99,14 @@ open_with2_main_win_create(Eo *__main_parent EINA_UNUSED)
    efl_gfx_size_hint_weight_set (current_app, 1.000000, 1.000000);
    efl_gfx_visible_set (current_app, EINA_TRUE);
    elm_object_part_text_set (current_app, NULL, "Label");
-   efl_gfx_size_set (current_app, 60, 30);
+   evas_object_move (current_app, 60, 30);
    elm_box_pack_end (elm_box3, elm_label1);
    elm_box_pack_end (elm_box3, current_app);
 
    search = elm_entry_add(elm_box1);
    pub_widgets->search = search;
    efl_gfx_visible_set (search, EINA_TRUE);
-   efl_gfx_size_set (search, 60, 30);
+   evas_object_move (search, 60, 30);
    efl_gfx_size_hint_weight_set (search, 1.000000, 0.000000);
    efl_gfx_size_hint_align_set (search, -1.000000, 0.000000);
    elm_entry_icon_visible_set (search, EINA_FALSE);
@@ -121,7 +121,7 @@ open_with2_main_win_create(Eo *__main_parent EINA_UNUSED)
    elm_icon1 = elm_icon_add(search);
    efl_gfx_size_hint_weight_set (elm_icon1, 1.000000, 1.000000);
    efl_gfx_visible_set (elm_icon1, EINA_TRUE);
-   efl_gfx_size_set (elm_icon1, 40, 40);
+   evas_object_move (elm_icon1, 40, 40);
    elm_icon_standard_set (elm_icon1, "search");
    elm_object_part_content_set(search, "icon", elm_icon1);
 
